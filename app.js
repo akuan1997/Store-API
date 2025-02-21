3e530b46ff5a1da1a1caf40e4baddccba2c82d75
akuan1997 <akuan10cc@gmail.com>
2025-02-21 18:38:24 +0800

    products 連線成功

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+const productRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -17,9 +18,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productRouter)
 // products route
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
