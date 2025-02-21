268f7d7db084f077ef94f21eb542d1a96278da1b
akuan1997 <akuan10cc@gmail.com>
2025-02-22 00:43:50 +0800

    改善getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -2,16 +2,30 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async error')  // testing
-    // const products = await Product.find({});  // 原本都是false，
+    // const products = await Product.find({});
     const products = await Product.find({
-        // featured:true,  // 全部都變true
+        // featured:true,
         name: 'vase table'
     });
     res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
     // console.log(req.query)  // 記得是query不是body
-    const products = await Product.find(req.query)
+    // const products = await Product.find(req.query)
+    const { featured, company, name } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = name
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
 }
 
--- populate.js
+++ populate.js
@@ -8,7 +8,7 @@ const jsonProducts = require('./products.json');
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        // await Product.deleteMany()  // 刪除Product集合中的所有資料
+        await Product.deleteMany()  // 刪除Product集合中的所有資料
         await Product.create(jsonProducts)  // 插入新的資料 (jsonProducts)
         console.log("DB Connected");
         // process.exit(0)
