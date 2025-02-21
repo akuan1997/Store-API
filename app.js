3064d67dec130f5b13f3afb54f04c8438ffd19a8
akuan1997 <akuan10cc@gmail.com>
2025-02-21 10:24:00 +0800

    start file ready

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+
+const port = process.env.PORT || 3000
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async (req, res) => {
+    try {
+        // connectDB
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
