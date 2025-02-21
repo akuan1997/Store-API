a0d4ab45d4b19534b7e0c7326a54a9c4cd710a50
akuan1997 <akuan10cc@gmail.com>
2025-02-21 18:28:01 +0800

    連結db

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -25,6 +27,7 @@ app.use(errorMiddleware)
 const start = async (req, res) => {
     try {
         // connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
             console.log(`Server started on port ${port}`)
         })
