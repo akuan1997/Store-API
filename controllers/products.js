06752341fded701bebf14679464cfb5b9bb642be
akuan1997 <akuan10cc@gmail.com>
2025-02-21 21:21:35 +0800

    query search

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,18 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async error')
-    // res.status(200).json({msg: 'products testing route'})
+    // throw new Error('testing async error')  // testing
+    // const products = await Product.find({});  // 原本都是false，
+    const products = await Product.find({
+        // featured:true,  // 全部都變true
+        name: 'vase table'
+    });
+    res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    // console.log(req.query)  // 記得是query不是body
+    const products = await Product.find(req.query)
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
