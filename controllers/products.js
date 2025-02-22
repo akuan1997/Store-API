75b495328dc7b97c5240ac183ea898f4427a2663
akuan1997 <akuan10cc@gmail.com>
2025-02-22 09:29:19 +0800

    只顯示特定屬性

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
         // search = 'a'，匹配任何含有a的name
         // $options: 'i' → 忽略大小寫（case-insensitive）
         // name: { $regex: search, $options: 'i'},
-    }).sort('price -name')
+    }).select('name price')
     // const products = await Product.find({
     //     // featured:true,
     //     name: 'vase table'
@@ -18,7 +18,7 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
     // console.log(req.query)  // 記得是query不是body
     // const products = await Product.find(req.query)
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -38,6 +38,10 @@ const getAllProducts = async (req, res) => {
         result = result.sort(sortList)
     } else {
         result = result.sort('createdAt')
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
     }
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
