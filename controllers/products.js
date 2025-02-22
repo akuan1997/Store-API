3e39bd47e6a8a232c08a9b0ec9da554249c592d5
akuan1997 <akuan10cc@gmail.com>
2025-02-22 11:49:27 +0800

    finished

--- controllers/products.js
+++ controllers/products.js
@@ -4,10 +4,12 @@ const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async error')  // testing
     // const products = await Product.find({});
     // const search = 'be'
-    const products = await Product.find({})
+    const products = await Product
+        .find({ price: { $gt: 108 } })  // price超過這個數字的才顯示
+        .sort('name')
         .select('name price')
-        .limit(3)
-        .skip(1)  // 跳過第一個，一樣會顯示三個 (limit=3)
+        // .limit(3)
+        // .skip(1)  // 跳過第一個，一樣會顯示三個 (limit=3)
     // const products = await Product.find({
     //     // featured:true,
     //     name: 'vase table'
@@ -17,7 +19,7 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
     // console.log(req.query)  // 記得是query不是body
     // const products = await Product.find(req.query)
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -29,7 +31,32 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i'}
     }
-    console.log(queryObject)
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        };
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+        console.log(filters);
+        const options = ['price', 'rating'];
+        filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+                if (!queryObject[field]) {
+                    queryObject[field] = {};  // 確保 field 是物件
+                }
+                queryObject[field][operator] = Number(value);  // 避免覆蓋
+            }
+        });
+    }
+    // console.log(queryObject)
     // const products = await Product.find(queryObject)
     let result = Product.find(queryObject)
     if (sort) {
